5f5f0a7726380ab401cbd8e2592d5e6ed1bfee12
Emmanuel Exiga <33707404+aPersonIsHere@users.noreply.github.com>
2023-11-06 13:13:55 -0600

    Updates the files to have some base code

---
+++ utils/helpers.js
@@ -0,0 +1,20 @@
+module.exports = {
+    format_date: (date) => {
+        return date.toLocaleDateString();
+    },
+
+    format_amount: (amount) => {
+        return parseInt(amount).toLocaleDateString();
+    },
+    get_emoji: () => {
+        const randomNum = Math.random();
+
+        if (randomNum > 0.7) {
+            return '<span for="img" aria-label="lightbulb">💡</span>';
+        } else if (randomNum > 0.4) {
+            return '<span for="img" aria-label="laptop">💻</span>';
+        } else {
+            return '<span for="img" aria-label="gear">⚙️</span>';
+        }
+    }
+};
